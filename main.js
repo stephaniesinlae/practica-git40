2705d8694a04fce4f6b28629927e7ccae5b170b3
Stephanie Sandoval <stephaniemsa96@gmail.com>
2020-05-08 18:57:03 -0600

    Modificamos main.js

--- main.js
+++ main.js
@@ -2,8 +2,8 @@
 // git init //nuevo repositorio
 // git status// para ver si hay archivos sin subir
 // git config --global user.name "nombre" // para decirle a git quienes somos
-
-// git add .
-// git commit -m "first commit"
+// git config --global user.email "email" // para decirle a git quienes somos
+// git add . // agregar los arachivos al staging area
+// git commit -m "comentario"
 // git remote add origin https://github.com/NOMBRE_USUARIO/NOMBRE_PROYECTO.git
 // git push -u origin master
