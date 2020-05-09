5279bd4aabf624be2512c6ac4d8773e8ffdf9430
Stephanie Sandoval <stephaniemsa96@gmail.com>
2020-05-08 18:52:52 -0600

    agregue main.js y readme.txt

---
+++ main.js
@@ -0,0 +1,9 @@
+// SUBIR REPOSITORIO A GITHUB
+// git init //nuevo repositorio
+// git status// para ver si hay archivos sin subir
+// git config --global user.name "nombre" // para decirle a git quienes somos
+
+// git add .
+// git commit -m "first commit"
+// git remote add origin https://github.com/NOMBRE_USUARIO/NOMBRE_PROYECTO.git
+// git push -u origin master
